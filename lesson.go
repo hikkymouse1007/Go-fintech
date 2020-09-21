6fdd7f7ca53f4f1c17d4b297588422c8c7273a73
hikkymouse1007 <mc.mdt1007it@gmail.com>
2020-09-21 23:26:16 +0900

    finish section18

--- lesson.go
+++ lesson.go
@@ -3,41 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]int{"apple": 100, "banana": 200}
-	fmt.Println(m)
-	fmt.Println(m["apple"])
-	// 新しいval
-	m["banana"] = 300
-	fmt.Println(m)
-	// 新しいkey_val
-	m["new"] = 500
-	fmt.Println(m)
+	// ASCIIの72, 73
+	b := []byte{72, 73}
+	fmt.Println(b)
+	// 72:H, 73:I
+	fmt.Println(string(b))
 
-	// 存在しないkey
-	fmt.Println(m["nothing"])
-
-	v, ok := m["apple"]
-	fmt.Println(v, ok)
-	//v := m["apple"]
-	//fmt.Println(v)
-
-	v2, ok2 := m["nothing"]
-	fmt.Println(v2, ok2)
-
-	m2 :=make(map[string]int)
-	m2["pc"] = 5000
-	fmt.Println(m2)
-
-	// nilのmap, memoryにmapがない
-	var m3 map[string]int
-	m3["pc"] = 5000
-	fmt.Println(m3)
-
-	var s []int
-	if s == nil{
-		fmt.Println("Nil")
-	}
-
-
-
+	c := []byte("HI")
+	fmt.Println(c)
+	fmt.Println(string(c))
 }
