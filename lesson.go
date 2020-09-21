7b7ff36f54199a408fcc7ecdda2ee5a8cb04be44
hikkymouse1007 <mc.mdt1007it@gmail.com>
2020-09-21 20:42:39 +0900

    finish section15

--- lesson.go
+++ lesson.go
@@ -3,17 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a)
+	n := []int{1,2,3,4,5,6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
 
-	/*var b [2]int = [2]int{100, 200}
-	b = append(b ,300) // appendできない
-	fmt.Println(b)
-	*/
+	n[2] = 100
+	fmt.Println(n)
 
-	var b []int = []int{100, 200}
-	b = append(b, 300)
-	fmt.Println(b)
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println(board)
+
+	n = append(n, 100, 200, 300, 400)
+	fmt.Println(n)
 }
