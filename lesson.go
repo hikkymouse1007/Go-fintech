9772c6c18ed3d0efa3d7a2490082e1e297479ae4
hikkymouse1007 <mc.mdt1007it@gmail.com>
2020-09-22 15:24:08 +0900

    finish secction22

--- lesson.go
+++ lesson.go
@@ -3,19 +3,17 @@ package main
 import "fmt"
 
 //可変長引数
-func foo(params ...int) {
-	fmt.Println(len(params), params)
-	for _, param := range params{
-		fmt.Println(param)
-	}
-}
 
 func main() {
-	foo()
-	foo(10, 20)
-	foo(10, 20, 30)
+	f := 1.11
+	int_f := int(f)
+	fmt.Printf("%T, %v\n", int_f, int_f)
 
-	s := []int{1, 2, 3}
-	fmt.Println(s)
-	foo(s...)
+	m := map[string]int{
+		"Mike":20,
+		"Nancy":24,
+		"Messi":30,
+	}
+
+	fmt.Printf("%T %v", m, m )
 }
