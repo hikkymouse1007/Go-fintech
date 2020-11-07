6e615b0f71da1d777536f6e15b4234ec1452c189
hikkymouse1007 <mc.mdt1007it@gmail.com>
2020-11-08 00:58:53 +0900

    finish section58

--- README.md
+++ README.md
@@ -1642,3 +1642,37 @@ func main() {
 // 出力
 {map[Key:20] {0 0}} 20
 ```
+
+## Sec-58
+### Tips
+Answer
+```
+package main
+
+import "fmt"
+
+func goroutine(s []string, c chan string){
+	sum := ""
+	for _, v := range s{
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
+
+func main(){
+	words := []string{"test1!", "test2!", "test3!", "test4!"}
+	c := make(chan string)
+	go goroutine(words, c)
+	for w := range c{
+		fmt.Println(w)
+	}
+}
+
+//出力
+test1!
+test1!test2!
+test1!test2!test3!
+test1!test2!test3!test4!
+
+```
--- lesson.go
+++ lesson.go
@@ -1,43 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-type Counter struct{
-	v map[string]int
-	mux sync.Mutex
+func goroutine(s []string, c chan string){
+	sum := ""
+	for _, v := range s{
+		sum += v
+		c <- sum
+	}
+	close(c)
 }
 
-func (c *Counter) Inc(key string){
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	c.v[key]++
-}
-
-func (c *Counter) Value(key string) int{
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	return c.v[key]
-}
-
-func main() {
-	//c := make(map[string]int)
-	c := Counter{v: make(map[string]int)}
-	go func() {
-		for i := 0; i < 10; i++{
-			//c["key"] += 1
-			c.Inc("Key")
-		}
-	}()
-	go func() {
-		for i := 0; i < 10; i++{
-			//c["key"] += 1
-			c.Inc("Key")
-		}
-	}()
-	time.Sleep(1 * time.Second)
-	fmt.Println(c, c.Value("Key"))
+func main(){
+	words := []string{"test1!", "test2!", "test3!", "test4!"}
+	c := make(chan string)
+	go goroutine(words, c)
+	for w := range c{
+		fmt.Println(w)
+	}
 }
