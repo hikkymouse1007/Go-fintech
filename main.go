57386a244b90fb1d376a674c363089d14a330299
hikkymouse1007 <mc.mdt1007it@gmail.com>
2020-11-10 00:03:37 +0900

    finish section64

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"awesomeProject/mylib"
-	"awesomeProject/mylib/under"
 	"fmt"
+	"github.com/markcheno/go-quote"
+	_ "github.com/markcheno/go-talib"
 )
 
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	fmt.Println(mylib.Average(s))
-
-	mylib.Say()
-	under.Hello()
-	person := mylib.Person{Name: "Mike", Age: 20}
-	fmt.Println(person)
-
-	fmt.Println(mylib.Public)
-	//fmt.Println(mylib.private) 実行できない
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
 }
