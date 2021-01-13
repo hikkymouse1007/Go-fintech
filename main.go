76897ac5049814db00cd3912dac6411ab073e7b3
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-01-13 23:57:43 +0900

    fisnish lesson 85

--- main.go
+++ main.go
@@ -1,97 +1,35 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-
-	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
 )
 
-var DbConnection *sql.DB
-
-type Person struct {
-	Name string
-	Age int
+type Page struct {
+	Title string
+	Body []byte
 }
 
-func main() {
-	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
-	defer DbConnection.Close()
-	cmd := `CREATE TABLE IF NOT EXISTS person(
-				name STRING,
-				age INT)`
-	_, err := DbConnection.Exec(cmd)
-	if err != nil {
-		log.Fatalln(err)
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil{
+		return nil, err
 	}
+	return &Page{Title: title, Body: body}, nil
+}
 
-	/* INSERT*/
-	//cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
-	//_, err = DbConnection.Exec(cmd, "Nancy", 20)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
+func main()  {
+	// ページの作成
+	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
+	p1.save()
 
-	/* UPDATE*/
-	//cmd = "UPDATE person SET age = ? WHERE name = ?"
-	//_, err = DbConnection.Exec(cmd, 25, "Mike")
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-
-	/* Multiple Select*/
-	//cmd = "SELECT * FROM PERSON"
-	//rows, _ := DbConnection.Query(cmd)
-	//defer rows.Close()
-	//var pp []Person
-	//for rows.Next() {
-	//	var p Person
-	//	err := rows.Scan(&p.Name, &p.Age)
-	//	if err != nil {
-	//		log.Println(err)
-	//	}
-	//	pp = append(pp, p)
-	//}
-	//for _, p := range pp {
-	//	fmt.Println(p.Name, p.Age)
-	//}
-
-	/*Single Select*/
-	//cmd = "SELECT * FROM person where age = ?"
-	//row := DbConnection.QueryRow(cmd, 20)
-	//var p Person
-	//err = row.Scan(&p.Name, &p.Age)
-	//if err != nil {
-	//	log.Println("No row")
-	//} else {
-	//	log.Println(err)
-	//}
-	//fmt.Println(p.Name, p.Age)
-
-	/*DELETE*/
-	//cmd = "DELETE FROM person WHERE name = ?"
-	//_, err = DbConnection.Exec(cmd, "Nancy")
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-
-	/*SQL Injection*/
-	//tableName := "person"
-	tableName := "person; INSERT INTO person (name, age) VALUES ('Mr.X', 100)" //SQLが実行されてしまう
-	cmd = fmt.Sprintf("SELECT * FROM %s", tableName) //tableNameは?による代入ができない, ?は通常文字列にエスケープされる
-	rows, _ := DbConnection.Query(cmd)
-	defer rows.Close()
-	var pp []Person //Personの構造体のみ入る配列を作成
-	for rows.Next() {
-		var p Person
-		err := rows.Scan(&p.Name, &p.Age)
-		if err != nil {
-			log.Println(err)
-		}
-		pp = append(pp, p)
-	}
-	for _, p := range pp {
-		fmt.Println(p.Name, p.Age)
-	}
+	//　ページのロード
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
 }
